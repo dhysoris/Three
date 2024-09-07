cd40d189e2c6b3dfdf0ad5d2ca8e2f50fc8db8b4
dhysoris <152842776+dhysoris@users.noreply.github.com>
2024-09-08 01:01:13 +0800

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,58 @@
+import * as THREE from "three"
+import {OrbitControls} from "jsm/controls/OrbitControls.js"
+import getStarfield from "./starfield.js"
+
+const height = window.innerHeight
+const width = window.innerWidth
+
+const scene = new THREE.Scene()
+
+const canvas = new THREE.WebGLRenderer({ antialias: true})
+canvas.setSize(width, height)
+document.body.appendChild(canvas.domElement)
+
+const fov = 75
+const aspect = width / height
+const near = 0.1
+const far = 1000
+
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far)
+camera.position.z = 3
+
+const geometry = new THREE.IcosahedronGeometry(1, 2)
+const material = new THREE.MeshStandardMaterial({
+    color: 0xffffff,
+    flatShading: true
+})
+
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+const wireMat = new THREE.MeshBasicMaterial({
+    color: 0xffffff,
+    wireframe: true
+})
+
+const meshWire = new THREE.Mesh(geometry, wireMat)
+meshWire.scale.setScalar(1.001)
+mesh.add(meshWire)
+
+const light = new THREE.HemisphereLight(0x000000, 0x111111)
+scene.add(light)
+
+const controls = new OrbitControls(camera, canvas.domElement)
+controls.enableDamping = true
+controls.dampingFactor = 0.03
+
+const stars = getStarfield({ numStars: 2000})
+scene.add(stars)
+
+function animate (t = 0) {
+    requestAnimationFrame(animate)
+    mesh.rotation.x = t * 0.0001
+    mesh.rotation.y = t * 0.0001
+    canvas.render(scene, camera)
+    controls.update()
+}
+
+animate()
